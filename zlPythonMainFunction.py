a7f760116fdc78844fc47ab22d736f725be5e7d8
gorf999 <33255926+gorf999@users.noreply.github.com>
2018-10-20 21:16:48 +0800

    Create zlPythonMainFunction.py

---
+++ zlPythonMainFunction.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+
+'''
+Copyright (C), 2017-2019, 张龙.
+FileName: zlBatchRenameFile.py
+Author: 张龙
+Version : 1.0
+Date: 2018-10-07
+Description: 张龙个人的批量重命名函数库
+Function List: # 主要函数及其功能
+1. -------
+History: // 历史修改记录
+<author>     <time>      <version >          <desc>
+  张龙      2018/10/07       1.0           build this moudle
+'''
+
+'''
+Number: 0000
+Function name: 主函数main().
+Description:程序启动主函数。
+Input Parameters: 无
+Output Parameters: 无
+Return Value: 无
+Depend On:无
+Others:
+Revision Date: 2018/09/24
+'''
+def main():
+    pass
+
+if __name__=='__main__':
+    print('Main Program Startup.')
+    main()
+else:
+    print('python_main_program is started as a module')
