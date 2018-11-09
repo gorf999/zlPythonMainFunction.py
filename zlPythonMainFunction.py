50064620cbc1dba4cdd11d147905656f2083b170
gorf999 <33255926+gorf999@users.noreply.github.com>
2018-11-09 10:36:54 +0800

    Update zlPythonMainFunction.py
    按照python语言的“PEP 257 -- Docstring Conventions”规范更新了注释样式。

--- zlPythonMainFunction.py
+++ zlPythonMainFunction.py
@@ -1,32 +1,39 @@
-#!/usr/bin/python3
-# -*- coding: UTF-8 -*-
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
 
 '''
-Copyright (C), 2017-2019, 张龙.
-FileName: zlBatchRenameFile.py
+FileName: zlPythonMainFunction.py
+Copyright (C), 2018-2019, 张龙.
 Author: 张龙
 Version : 1.0
-Date: 2018-10-07
-Description: python主函数main().
-Function List: # 主要函数及其功能
-1. -------
-History: // 历史修改记录
-<author>     <time>      <version >          <desc>
-  张龙      2018/10/07       1.0           build this moudle
+Date: 2018-11-08
+Description:
+Data List:
+    1.
+Function List: 
+    1.
+Class List:
+    1.
+Usage:
+History: 
+    <version >        <Date>        <author>            <desc>
+       1.0           2018/10/07       张龙          build this moudle
 '''
 
-'''
-Number: 0000
-Function name: 主函数main().
-Description:程序启动主函数。
-Input Parameters: 无
-Output Parameters: 无
-Return Value: 无
-Depend On:无
-Others:
-Revision Date: 2018/09/24
-'''
+
 def main():
+    '''
+    Number: 0000
+    Function name: 主函数main().
+    Description:程序启动主函数。
+    Input Parameters: 无
+    Output Parameters: 无
+    Return Value: 无
+    Depend On:无
+    Others:
+    Revision Date: 2018/09/24
+    '''
     pass
 
 if __name__=='__main__':
